129816abfcd3cd17a1f8f6259cd1ad79d5769266
Murat Memis <h.m.f.m.92@gmail.com>
2021-10-29 15:29:42 +0200

    features

--- app.js
+++ app.js
@@ -23,33 +23,56 @@ let hangman_words = [
 ]
 
 window.onload = function () {
-  // change to queryselector later
-  // let hangman_word = document.getElementById('answers')
-  let letters = document.getElementById('letters')
+  let hangman_word = document.getElementById('answers')
+  let letters = document.querySelector('#letters')
+  //Change the userLife to correspond with the amount of life a hangman needs
   let userLife = 10
+
+  document.querySelector('#lives').innerText = 'Amount of lives left: ' + userLife
 
   let words = hangman_words[Math.floor(Math.random() * hangman_words.length)]
   console.log(words)
 
   // if the letter exists in the word, it should appear on the user's screen, depending on the position and number of the letter.
   document.querySelector('#a').onclick = function () {
-    if (words.includes('a')) {
-    } else {
-      userLife--
+    if (userLife >= 0) {
+      document.querySelector('#lives').innerText = 'Amount of lives left: ' + userLife
+
+      if (words.includes('a')) {
+        letters.innerText = 'a'
+      } else {
+        userLife--
+      }
+    } else if (userLife <= 0) {
+      document.querySelector('#lives').innerText = 'You have lost'
     }
   }
 
   document.querySelector('#b').onclick = function () {
-    if (words.includes('b')) {
-    } else {
-      userLife--
+    if (userLife >= 0) {
+      document.querySelector('#lives').innerText = 'Amount of lives left: ' + userLife
+
+      if (words.includes('b')) {
+        letters.innerText = 'b'
+      } else {
+        userLife--
+      }
+    } else if (userLife <= 0) {
+      document.querySelector('#lives').innerText = 'You have lost'
     }
   }
 
   document.querySelector('#c').onclick = function () {
-    if (words.includes('c')) {
-    } else {
-      userLife--
+    if (userLife >= 0) {
+      document.querySelector('#lives').innerText = 'Amount of lives left: ' + userLife
+
+      if (words.includes('c')) {
+        letters.innerText = 'c'
+      } else {
+        userLife--
+      }
+    } else if (userLife <= 0) {
+      document.querySelector('#lives').innerText = 'You have lost'
     }
   }
 }
