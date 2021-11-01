4d497bcff4f03ec7c7e33b2dd3adac99a856d376
Murat Memis <h.m.f.m.92@gmail.com>
2021-11-01 14:04:05 +0100

    attempting to add functionality and features
    Did not succeed this time :(

--- app.js
+++ app.js
@@ -1,37 +1,62 @@
-let hangman_words = [
-  'able',
-  'about',
-  'account',
-  'acid',
-  'across',
-  'act',
-  'addition',
-  'adjustment',
-  'advertisement',
-  'after',
-  'again',
-  'against',
-  'agreement',
-  'air',
-  'all',
-  'almost',
-  'among',
-  'amount',
-  'amusement',
-  'and',
-  'Adi',
-]
-
 window.onload = function () {
-  let hangman_word = document.getElementById('answers')
+  let hangman_words = [
+    'able',
+    'about',
+    'account',
+    'acid',
+    'across',
+    'act',
+    'addition',
+    'adjustment',
+    'advertisement',
+    'after',
+    'again',
+    'against',
+    'agreement',
+    'air',
+    'all',
+    'almost',
+    'among',
+    'amount',
+    'amusement',
+    'and',
+    'Adi',
+  ]
+
   let letters = document.querySelector('#letters')
+  // let hiddenWord = []
+  // let answerString = 'Test'
+  let words = hangman_words[Math.floor(Math.random() * hangman_words.length)]
+  console.log(words)
+
+  //Logic to make the word appear with underscores
+
+  // for (var i = 0; i < words; i++) {
+  //   hiddenWord[i] = '_'
+  // }
+
+  // answerString = hiddenWord.join(' ')
+  // document.getElementById('answers').innerText = answerString
+
+  //Got sidetracked trying to figure out how to make an array list the buttons instead of raw HTML code
+
+  // let hangman_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+  // var generateKeys = function (keys) {
+  //   for (var i = 0; i < keys.length; i++) {
+  //     for (var j = 0; j < keys[i].length; j++) {
+  //       var input = document.getElementById('myInput')
+  //       input.value = keys[i][j]
+  //     }
+  //   }
+  // }
+
+  // generateKeys(hangman_letters)
+
   //Change the userLife to correspond with the amount of life a hangman needs
   let userLife = 10
 
   document.querySelector('#lives').innerText = 'Amount of lives left: ' + userLife
-
-  let words = hangman_words[Math.floor(Math.random() * hangman_words.length)]
-  console.log(words)
 
   // if the letter exists in the word, it should appear on the user's screen, depending on the position and number of the letter.
   document.querySelector('#a').onclick = function () {
