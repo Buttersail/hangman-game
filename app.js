28676273b69523bd4bb9a505ad44741f6c28824f
Murat Memis <h.m.f.m.92@gmail.com>
2021-11-07 14:06:14 +0100

    adis session
    session with Adi

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 window.onload = function () {
-  let hangmanWords = ['able', 'about', 'acne', 'adi']
+  let hangmanWords = ['able', 'about', 'acne', 'adi', 'amba']
 
-  let letters = document.querySelector('#letters')
   let hiddenWord = []
   let answerString
-  let splitWords = []
-  let words = hangmanWords[Math.floor(Math.random() * hangmanWords.length)]
-  console.log(words)
+  let selectedWord = hangmanWords[Math.floor(Math.random() * hangmanWords.length)]
+  console.log(selectedWord)
 
   //Logic to make the word appear with underscores
-  for (var i = 0; i < words.length; i++) {
+  for (var i = 0; i < selectedWord.length; i++) {
     hiddenWord[i] = '_'
   }
 
   answerString = hiddenWord.join(' ')
   document.querySelector('#hiddenWord').innerText = answerString
 
-  //Splitting the words into singular letters
-  splitWords = words.split('')
-  console.log(splitWords)
+  //Adi logic :D
+  let letter = ''
+  for (var i = 0; i < selectedWord.length; i++) {
+    if (selectedWord[i] === 'a') {
+      letter = letter + 'a'
+    } else {
+      letter = letter + '_'
+    }
+  }
+  console.log(letter)
 
   //Change the userLife to correspond with the amount of life a hangman needs
   let userLife = 10
@@ -27,24 +32,14 @@ window.onload = function () {
 
   // if the letter exists in the word, it should appear on the user's screen, depending on the position and number of the letter.
   document.querySelector('#a').onclick = function () {
-    for (var i = 0; i < splitWords.length; i++) {
+    for (var i = 0; i < selectedWord.length; i++) {
       if (userLife >= 0) {
         document.querySelector('#lives').innerText = 'Amount of lives left: ' + userLife
 
-        if (words.includes(splitWords[i])) {
+        if (selectedWord.includes('a')) {
           //Have to figure out a way to make this logic works as intended.
           //Doesn't put the letter in the right spot, just fills out the word currently
-          // if (splitWords.includes('a')) {
-          //   console.log('tst')
-          // }
-
-          var result = words.split('').map(function (l, i) {
-            return words.slice(-i)
-          })
-
-          console.log(result)
-
-          document.querySelector('#hiddenWord').innerText = result
+          document.querySelector('#hiddenWord').innerText = 'a'
         } else {
           userLife--
         }
@@ -58,7 +53,7 @@ window.onload = function () {
     if (userLife >= 0) {
       document.querySelector('#lives').innerText = 'Amount of lives left: ' + userLife
 
-      if (splitWords.includes('b')) {
+      if (selectedWord.includes('b')) {
         document.querySelector('#hiddenWord').innerText = 'b'
       } else {
         userLife--
@@ -72,7 +67,7 @@ window.onload = function () {
     if (userLife >= 0) {
       document.querySelector('#lives').innerText = 'Amount of lives left: ' + userLife
 
-      if (splitWords.includes('c')) {
+      if (selectedWord.includes('c')) {
         document.querySelector('#hiddenWord').innerText = 'c'
       } else {
         userLife--
@@ -86,7 +81,7 @@ window.onload = function () {
     if (userLife >= 0) {
       document.querySelector('#lives').innerText = 'Amount of lives left: ' + userLife
 
-      if (splitWords.includes('d')) {
+      if (selectedWord.includes('d')) {
         document.querySelector('#hiddenWord').innerText = 'd'
       } else {
         userLife--
